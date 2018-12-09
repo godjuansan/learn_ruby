32b7a8a5d41706b2ff463bb46f16239bc4a68b0f
Otto Alexander <godjuansan88@gmail.com>
2018-12-09 21:27:10 +0700

    05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+
+	def title
+		x = @title.split(" ")
+		except = ["and", 'over', 'the', 'a', 'an', 'is', 'in', 'of', 'at']
+		x.each do |y|
+			y.capitalize! unless except.include? y
+		end
+		x[0].capitalize!
+		@title = x.join(" ")
+	end
 end
