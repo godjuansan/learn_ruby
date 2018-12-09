8709da33a0c22f721ed71a020747495e8d6e3d49
Otto Alexander <godjuansan88@gmail.com>
2018-12-09 16:40:20 +0700

    02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add(x,y)
+	x+y
+end
+def subtract(x,y)
+	x-y
+end
+def sum(x)
+	if (x == [])
+		0
+	else
+		count = 0
+		x.each do |u|
+			count += u
+		end
+		count
+	end
+end
+def multiply(x,y)
+	x*y
+end
+def power(x,y)
+	x**y
+end
+def factorial(x)
+	if x == 0
+		1
+	else
+		count = 1
+		i = 1
+		while i <= x
+			count *= i
+			i += 1
+		end
+		count
+	end
+end
